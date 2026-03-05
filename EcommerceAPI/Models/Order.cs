dec1511b1a786afe0b5033d4da3a30d8553a14e5
Lukas Olsson <Lukas99O@hotmail.com>
2026-03-05 10:08:07 +0100

    Setting up database part 1

---
+++ EcommerceAPI/Models/Order.cs
@@ -0,0 +1,24 @@
+﻿namespace EcommerceAPI.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public string Email { get; set; } = null!;
+        public string PhoneNumber { get; set; } = null!;
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string StreetAddress { get; set; } = null!;
+        public string City { get; set; } = null!;
+        public int ZipCode { get; set; }
+
+        public string? OrganizationNumber { get; set; }
+        public string? CompanyName { get; set; }
+        public string? OrderMarking { get; set; }
+        public string? InvoiceEmail { get; set; }
+
+        public int ProductId { get; set; }
+        public ICollection<Product>? Products { get; set; }
+    }
+}
---
+++ EcommerceAPI/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EcommerceAPI.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public decimal Price { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public int GenreId { get; set; }
+        public Genre? Genre { get; set; }
+    }
+}
